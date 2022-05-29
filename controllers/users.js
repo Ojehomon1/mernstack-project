289d1d4fdd93c296a81821111c66d3fee7b19b44
Ojehomon1 <periju@web.de>
2022-05-29 13:37:42 +0200

    signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,31 @@
-// function for login
-// function for signup
+import User from "../models/user.js";
+
 
 const login = (req, res) => {
   res.send("welcome to login page");
 };
-const signup = (req, res) => {
-  res.send({ msg: "welcome to signup ", data: req.body });
+
+const signup = async(req, res) => {
+    // username , email , password , confirm password
+    const {username, email , password,confirmPassword} = req.body
+    try {
+        const oldUser = await User.findOne({email});
+        if(oldUser){
+          return  res.status(400).json({msg : "This email already exist please use different one! "})
+        }
+        if(password === confirmPassword){
+            const user = await User.create({
+                username,
+                email,
+                password
+            })
+            res.status(201).json({msg : "user created :) ",data :user})
+        }
+        
+    } catch (error) {
+        res.status(500).json({msg:"somthing went wrong!"});
+    }
+  //res.send({ msg: "welcome to signup ", data: req.body });
 };
 
 export { login, signup };
